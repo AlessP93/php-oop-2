b21875aaaabbe6b020311a328bf7629e7eeb7fe5
AlessP93 <alespiscino@libero.it>
2022-06-28 18:23:28 +0200

    aggiunto extends

--- classes/registeredUser.php
+++ classes/registeredUser.php
@@ -1,4 +1,14 @@
 <?php
+require_once __DIR__ . '/classes/creditCrad.php';
 
+class User extends CreditCard{
+    public $discount = 20;
+
+
+    public function __construct($discount)
+    {
+        $this->discount = $_discount;
+    }
+}
 
 ?>
